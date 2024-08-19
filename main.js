bab4fd1c285fc5354596b49988789e01cf32b293
Kalzaf <kalzaf50@gmail.com>
2024-08-19 18:17:32 +0800

    add song

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
+
+// JavaScript to show the flowers when the button is clicked
+document.getElementById('showFlowersBtn').addEventListener('click', function() {
+        document.querySelector('.flowers').style.display = 'block';
+        document.querySelector('.button').style.display = 'none';
+        var audio = document.getElementById('flowerSong');
+        audio.play();
+});
