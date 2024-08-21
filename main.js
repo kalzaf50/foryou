997c212f9916b599d105aacdca02f1a4ac85c97d
Kalzaf <kalzaf50@gmail.com>
2024-08-21 17:44:40 +0800

    asdas

--- main.js
+++ main.js
@@ -4,11 +4,35 @@ onload = () =>{
 
 // JavaScript to show the flowers when the button is clicked
 document.getElementById('showFlowersBtn').addEventListener('click', function() {
-        var waterDrop = document.getElementById('waterDrop');
-        waterDrop.classList.add('drop-animation');
+        var waterDrop1 = document.getElementById('waterDrop1');
+        var waterDrop2 = document.getElementById('waterDrop2');
+        var waterDrop3 = document.getElementById('waterDrop3');
+        var waterDrop4 = document.getElementById('waterDrop4');
         var audio = document.getElementById('flowerSong');
+        
+        // Start the drop animation for the first image
+        waterDrop1.classList.add('drop-animation');
+    
         setTimeout(function() {
-                audio.play();
-                document.querySelector('.flowers').style.display = 'block';
-            }, 1000); // Duration of the drop animation in milliseconds
-});
+            waterDrop1.style.opacity = 0; // Hide the first image
+            waterDrop2.style.opacity = 1; // Show the second image
+            audio.play(); // Play audio
+        }, 1100); // Duration of the drop animation in milliseconds
+        
+        // Show the third image after 0.5 seconds of the second image appearing
+        setTimeout(function() {
+            waterDrop2.style.opacity = 0; // Hide the second image
+            waterDrop3.style.opacity = 1; // Show the third image
+        }, 1140); // 1000ms for the first image drop + 500ms delay
+    
+        // Show the fourth image after 0.5 seconds of the third image appearing
+        setTimeout(function() {
+            waterDrop3.style.opacity = 0; // Hide the third image
+            waterDrop4.style.opacity = 1; // Show the fourth image
+            document.querySelector('.flowers').style.display = 'block';
+        }, 1180); // 1500ms for the second image transition + 500ms delay
+
+        setTimeout(function() {
+                waterDrop4.style.opacity = 0; // Hide the third image
+            }, 1220); // 1500ms for the second image transition + 500ms delay
+    });
