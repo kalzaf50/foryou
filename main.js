f6ea6de62cdd715b5f3abc1036efe773ab0914f4
Kalzaf <kalzaf50@gmail.com>
2024-08-21 23:05:42 +0800

    asd

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ document.getElementById('showFlowersBtn').addEventListener('click', function() {
         var waterDrop2 = document.getElementById('waterDrop2');
         var waterDrop3 = document.getElementById('waterDrop3');
         var waterDrop4 = document.getElementById('waterDrop4');
+        var waterDrop5 = document.getElementById('waterDrop5');
         var audio = document.getElementById('flowerSong');
         
         // Start the drop animation for the first image
@@ -23,16 +24,26 @@ document.getElementById('showFlowersBtn').addEventListener('click', function() {
         setTimeout(function() {
             waterDrop2.style.opacity = 0; // Hide the second image
             waterDrop3.style.opacity = 1; // Show the third image
-        }, 1140); // 1000ms for the first image drop + 500ms delay
-    
+        }, 1150); 
         // Show the fourth image after 0.5 seconds of the third image appearing
         setTimeout(function() {
             waterDrop3.style.opacity = 0; // Hide the third image
             waterDrop4.style.opacity = 1; // Show the fourth image
-            document.querySelector('.flowers').style.display = 'block';
-        }, 1180); // 1500ms for the second image transition + 500ms delay
+        }, 1200); 
 
         setTimeout(function() {
                 waterDrop4.style.opacity = 0; // Hide the third image
-            }, 1220); // 1500ms for the second image transition + 500ms delay
+                waterDrop5.style.opacity = 1; // Show the fourth image
+        }, 1250); 
+        
+        setTimeout(function() {
+                waterDrop5.style.opacity = 0; // Hide the third image
+                waterDrop6.style.opacity = 1; // Show the fourth image
+                document.querySelector('.flowers').style.display = 'block';
+        }, 1300);
+
+        setTimeout(function() {
+                waterDrop6.style.opacity = 0; // Hide the third image
+                document.querySelector('.flowers').style.display = 'block';
+        }, 1400); 
     });
